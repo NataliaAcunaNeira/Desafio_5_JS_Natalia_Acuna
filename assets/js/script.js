1d18a3fc097b97f9486ba7c8d25bfe4b25572cf2
Natalia Acuña <natelizau@gmail.com>
2023-10-13 21:08:49 -0300

    elimina js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
 
 
 
-//Ejercicio 2.2
-
-function pintar(elemento) {
-    elemento.style.backgroundColor = 'yellow'
-}
-
-const ele = document.getElementById("ele1")
-ele.addEventListener("click", function () {
-    pintar(ele)
-});
-
-//Ejercicio 2.3
-
-function cambiarColor(pintar, color) {
-    pintar.style.backgroundColor = color
-}
-
-pintar(ele)
-cambiarColor(ele, 'green')
 
 
 
